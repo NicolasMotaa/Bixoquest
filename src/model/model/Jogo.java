9588dba1ab9a7cb7eda85476fa73bc48b8557e2c
NicolasMotaa <nicolasdocarmo11@gmail.com>
2026-04-12 14:30:30 -0300

    Definição e Repositório de partidas (Jogo)

---
+++ src/model/model/Jogo.java
@@ -0,0 +1,16 @@
+package model.model;
+
+public class Jogo {
+    private Jogador j;
+    private Tempo tempo;
+    private final int id; //no service vai chamar nextId do repositório
+    public Jogo(Jogador j, Tempo tempo) {
+        this.j = j;
+        this.tempo = tempo;
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
---
+++ src/model/repository/JogoRepository.java
@@ -0,0 +1,36 @@
+package model.repository;
+
+import model.model.Jogador;
+import model.model.Jogo;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class JogoRepository {
+    private final Map<Integer, Jogo> bancoDeJogos = new LinkedHashMap<>();
+    private final AtomicInteger id = new AtomicInteger(0);
+
+    public int gerarId(){ return id.incrementAndGet(); }
+
+    public Jogo salvar(Jogo jogo) {
+        bancoDeJogos.put(jogo.getId(), jogo);
+        return jogo;
+    }
+    public Jogo buscarJogo(int id){
+        return bancoDeJogos.get(id);
+    }
+
+    public List<Jogo> listarJogos(){
+        return new ArrayList<>(bancoDeJogos.values());
+    }
+
+    public boolean deletarJogo(int id) {
+        return (bancoDeJogos.remove(id)) != null;
+    }
+
+
+
+}
